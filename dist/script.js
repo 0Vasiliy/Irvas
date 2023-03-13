b22f208e504435d73db962140f651b2840dd3432
Козин-Поздняков Василий <vasiliy_pit@mail.ru>
2023-03-13 20:44:20 +0300

    modal window close

--- dist/script.js
+++ dist/script.js
@@ -14200,9 +14200,11 @@ __webpack_require__.r(__webpack_exports__);
 const modals = () => {
   //Передача селекторов и получение переменных
   function bindModal(triggerSelector, modalSelector, closeSelector) {
+    let closeClickOverlay = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : true;
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector);
+      close = document.querySelector(closeSelector),
+      window = document.querySelectorAll('[data-modal]');
 
     // Передача в функцию несколько элементов и вешается обработчик
     trigger.forEach(item => {
@@ -14210,6 +14212,10 @@ const modals = () => {
         if (e.target) {
           e.preventDefault();
         }
+        //закрываются все модальные окна
+        window.forEach(item => {
+          item.style.display = "none";
+        });
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
       });
@@ -14217,13 +14223,21 @@ const modals = () => {
 
     // Закрытие modal
     close.addEventListener('click', () => {
+      //закрываются все модальные окна
+      window.forEach(item => {
+        item.style.display = "none";
+      });
       modal.style.display = "none";
       document.body.style.overflow = "";
     });
 
     // Закрытие modal при клике вне модалного окна
     modal.addEventListener('click', e => {
-      if (e.target === modal) {
+      if (e.target === modal && closeClickOverlay) {
+        //закрываются все модальные окна
+        window.forEach(item => {
+          item.style.display = "none";
+        });
         modal.style.display = "none";
         document.body.style.overflow = "";
       }
@@ -14240,6 +14254,8 @@ const modals = () => {
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+  bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
   showModalByTime('.popup', 60000);
 };
 /* harmony default export */ __webpack_exports__["default"] = (modals);
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,17 +1,23 @@
 // modal
 const modals = () => {
     //Передача селекторов и получение переменных
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector,closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
-              close = document.querySelector(closeSelector);
+              close = document.querySelector(closeSelector),
+              window = document.querySelectorAll('[data-modal]');
 
         // Передача в функцию несколько элементов и вешается обработчик
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault();
-                }     
+                } 
+                //закрываются все модальные окна
+                window.forEach(item =>{
+                    item.style.display = "none";
+                })
+                
                 modal.style.display = "block";
                 document.body.style.overflow = "hidden";
             });
@@ -19,13 +25,21 @@ const modals = () => {
 
          // Закрытие modal
         close.addEventListener('click', () => {
+            //закрываются все модальные окна
+            window.forEach(item =>{
+                item.style.display = "none";
+            })
             modal.style.display = "none";
             document.body.style.overflow = "";
         });
 
         // Закрытие modal при клике вне модалного окна
         modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
+                //закрываются все модальные окна
+                window.forEach(item =>{
+                    item.style.display = "none";
+                })
                 modal.style.display = "none";
                 document.body.style.overflow = ""; 
             }
@@ -43,6 +57,8 @@ const modals = () => {
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close',false);
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close',false);
     showModalByTime('.popup', 60000);
 };
 
